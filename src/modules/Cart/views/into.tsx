313679fe0435705ef00be18dbc1a2b403216d636
mennah <mennah@mashkor.com>
2023-05-07 00:43:14 +0300

    into message

---
+++ src/modules/Cart/views/into.tsx
@@ -0,0 +1,14 @@
+export const Into = () => {
+  return (
+    <>
+      <div>
+        <p className="font-bold text-lg">
+          Welcome, Mena
+        </p>
+        <p className="text-gray-500 text-xs">
+          Discover watever you need easily
+        </p>
+      </div>
+    </>
+  )
+}
