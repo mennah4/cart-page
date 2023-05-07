ecf44f72118f83901c7bc0c076f8718775bb0fe9
mennah <mennah@mashkor.com>
2023-05-07 03:13:38 +0300

    cart summary

--- src/modules/Cart/cart.page.tsx
+++ src/modules/Cart/cart.page.tsx
@@ -6,6 +6,7 @@ import { Into } from "./views/into";
 import { Search } from "./views/searchbar";
 import CartItem from "./views/item";
 import SettingsIcon from "../../assets/icons/settings";
+import { CartSummary } from "./views/summary";
 
 const Cart = () => {
   const cartItems = useSelector((store: any) => store['CART_STORE_NAME'].items);
@@ -29,9 +30,13 @@ const Cart = () => {
         <div className="flex justify-between m-3 p-3">
           <p className="font-semibold text-xl">Current Order</p>
           <button className="flex items-center h-10 w-12 mx-1 justify-center rounded-lg p-1 text-sm font-medium bg-gray-100 hover:bg-orange-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-          <SettingsIcon />
-        </button>
+            <SettingsIcon />
+            
+          </button>
         </div>
+        <div className="mt-3">
+              <CartSummary />
+            </div>
       </div>
     </div>
 
---
+++ src/modules/Cart/views/summary.tsx
@@ -0,0 +1,47 @@
+export const CartSummary = () => {
+  return (
+    <>
+      <div className="flex items-center px-2 py-5 rounded-lg">
+        <div className="flex w-4/5 rounded-lg">
+          <div className="w-36">
+            <img className="h-24 rounded-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5w5GHVgYsOeLS0uzEU__2WPD9igNhKghLlg&usqp=CAU" alt="" />
+          </div>
+          <div className="flex flex-col justify-between flex-grow">
+            <div className="py-2 flex w-full justify-between">
+              <p className="font-bold text-lg line-clamp-2">Plain Croissant</p>
+            </div>
+            <div className="-pt-3 flex justify-between items-center">
+              <p className="font-[600] text-orange-500 text-xl">$3.2</p>
+              <div className="flex items-center gap-2">
+                <button className="text-white flex items-center justify-center h-4 w-4 rounded-md p-3 text-sm font-medium bg-orange-500  focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">+</button>
+                1
+                <button className="text-white flex items-center justify-center h-4 w-4 rounded-md p-3 text-sm font-medium bg-orange-500  focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">-</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center px-2 py-5 rounded-lg">
+        <div className="flex w-4/5 rounded-lg">
+          <div className="w-36">
+            <img className="h-24 rounded-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5w5GHVgYsOeLS0uzEU__2WPD9igNhKghLlg&usqp=CAU" alt="" />
+          </div>
+          <div className="flex flex-col justify-between flex-grow">
+            <div className="py-2 flex w-full justify-between">
+              <p className="font-bold text-lg line-clamp-2">Plain Croissant</p>
+            </div>
+            <div className="-pt-3 flex justify-between items-center">
+              <p className="font-[600] text-orange-500 text-xl">$3.2</p>
+              <div className="flex items-center gap-2">
+                <button className="text-white flex items-center justify-center h-4 w-4 rounded-md p-3 text-sm font-medium bg-orange-500  focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">+</button>
+                1
+                <button className="text-white flex items-center justify-center h-4 w-4 rounded-md p-3 text-sm font-medium bg-orange-200  focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">-</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
+}
